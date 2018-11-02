21355a18d6227341ef991adc30e94a948e614bed
Rishi Mahadevan <rishimahadevan@gmail.com>
2018-11-02 20:24:44 +0800

    Update turning code

--- turning_code_actual/turning_code_actual.ino
+++ turning_code_actual/turning_code_actual.ino
@@ -3,41 +3,104 @@
 #include <SoftwareSerial.h>
 #include <stdbool.h>
 #include <MeMCore.h>
-
 MeDCMotor motor_9(9);
 MeDCMotor motor_10(10);
-
 bool isButtonPressed = false;
+MeUltrasonicSensor ultrasonic_4(4);
+MeLineFollower linefollower_2(2);
+//turn left 
+void left_turn(){ 
+  move(3, 100);
+  delay(600);     
+  move(1,0);
+}
+// turn right
+void right_turn(){ 
+  move(4, 100);
+  delay(600);     
+  move(1,0);
+}
 
-void move(int direction, int speed){
-  int leftSpeed = 0;
-  int rightSpeed = 0;
-  //to stop, pass in a speed val of 0
-  if(direction == 1){ //go forward
-    leftSpeed = speed;
-    rightSpeed = speed;  
-  } else if(direction == 2){ //go reverse
-    leftSpeed = -speed;
-    rightSpeed = -speed;  
-  } else if(direction == 3){//turn left
-    leftSpeed = -speed;
-    rightSpeed = speed;  
-  } else if(direction == 4){//turn right
-    leftSpeed = speed;
-    rightSpeed = -speed;
+//turn 180 right
+void right_turn_turn(){ 
+  move(4, 100);
+  delay(600);     
+  move(4,100);
+  delay(600); 
+  move(1,0);
+}
+
+//turn 180 left 
+void left_turn_turn(){ 
+  move(3, 100);
+  delay(500);     
+  move(3,100);
+  delay(500); 
+  move(1,0);
+}
+
+//turn succ_right_turn
+void succ_right_turn(){ 
+  move(4, 100);
+  delay(600);  
+  move(1, 0);
+  
+  while((ultrasonic_4.distanceCm()) > (8)){ 
+    move(1,100);
   }
-  motor_9.run((9)==M1?(-leftSpeed):(leftSpeed));
-  motor_10.run((10)==M1?(-rightSpeed):(rightSpeed));
+  
+  move(1,0); 
+  
+  move(4,100);
+  delay(600); 
+  move(1,0);
+}
+void succ_left_turn(){ 
+  move(3, 100);
+  delay(500);  
+  move(1, 0);
+  while((ultrasonic_4.distanceCm()) > (8)){ 
+    move(1,100);
+  }
+  move(1,0); 
+  move(3,100);
+  delay(500); 
+  move(1,0);
+}
+void move(int direction, int speed)
+{
+      int leftSpeed = 0;
+      int rightSpeed = 0;
+      if(direction == 1){
+          leftSpeed = speed;
+          rightSpeed = speed;
+      }else if(direction == 2){
+          leftSpeed = -speed;
+          rightSpeed = -speed;
+      }else if(direction == 3){
+          leftSpeed = -speed;
+          rightSpeed = speed;
+      }else if(direction == 4){
+          leftSpeed = speed;
+          rightSpeed = -speed;
+      }
+      motor_9.run((9)==M1?-(leftSpeed):(leftSpeed));
+      motor_10.run((10)==M1?-(rightSpeed):(rightSpeed));
 }
 void setup() {
   // put your setup code here, to run once:
-  pinMode(A7, INPUT);
-      move(3, 100);
-    delay(1000);
-    move(1, 60);
+  //succ_left_turn;
+  Serial.println(linefollower_2.readSensors());
+  
 }
 
 void loop() {
+  if(((linefollower_2.readSensors())==(0))){
+          succ_left_turn();
+          //move(4,100);
+          //delay(100);
+          //move(1,0);
+      }
 //  // put your main code here, to run repeatedly:
 //    if((0^(analogRead(A7)>10?0:1)) && !isButtonPressed){
 //        move(1,100);
